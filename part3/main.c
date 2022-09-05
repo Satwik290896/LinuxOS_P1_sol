9efc91e0b8ec3ad0ea83ec1833347856ceebf6bd
Sparrow <1158443037@qq.com>
2022-09-05 13:48:33 -0400

    Update part3/main.c

--- part3/main.c
+++ part3/main.c
@@ -1,19 +1,66 @@
 void main() {
     __asm__("mov $0xb800, %ax");
     __asm__("mov %ax, %ds");
-    int i;
     char s[] = {'h','e','l','l','o',',',' ','w','o','r','l','d'};
     char *vga_con = (char *)0;
-    for (i = 0; i < 12; ++i) {
-        __asm__("mov $0, %ax");
-        __asm__("mov %ax, %ds");
-        char c = s[i];
-        __asm__("mov $0xb800, %ax");
-        __asm__("mov %ax, %ds");
-        *vga_con = c;
-        vga_con++;
-        *vga_con = (char)0xf0;
-        vga_con++;
-    }
+    *vga_con = s[0];
+    vga_con++;
+    *vga_con = (char)0xf0;
+    vga_con++;
+
+    *vga_con = s[1];
+    vga_con++;
+    *vga_con = (char)0xf0;
+    vga_con++;
+
+    *vga_con = s[2];
+    vga_con++;
+    *vga_con = (char)0xf0;
+    vga_con++;
+
+    *vga_con = s[3];
+    vga_con++;
+    *vga_con = (char)0xf0;
+    vga_con++;
+
+    *vga_con = s[4];
+    vga_con++;
+    *vga_con = (char)0xf0;
+    vga_con++;
+
+    *vga_con = s[5];
+    vga_con++;
+    *vga_con = (char)0xf0;
+    vga_con++;
+
+    *vga_con = s[6];
+    vga_con++;
+    *vga_con = (char)0xf0;
+    vga_con++;
+
+    *vga_con = s[7];
+    vga_con++;
+    *vga_con = (char)0xf0;
+    vga_con++;
+
+    *vga_con = s[8];
+    vga_con++;
+    *vga_con = (char)0xf0;
+    vga_con++;
+
+    *vga_con = s[9];
+    vga_con++;
+    *vga_con = (char)0xf0;
+    vga_con++;
+
+    *vga_con = s[10];
+    vga_con++;
+    *vga_con = (char)0xf0;
+    vga_con++;
+
+    *vga_con = s[11];
+    vga_con++;
+    *vga_con = (char)0xf0;
+
     while (1);
 }
