06a67f1a26cc0fe800d7cfc26e9c00b72b2dae92
Sparrow <1158443037@qq.com>
2022-09-05 15:06:52 -0400

    Update part3/main.c

--- part3/main.c
+++ part3/main.c
@@ -1,3 +1,5 @@
+#define WHITE_BG 0xf0
+
 void main() {
     __asm__("mov $0xb800, %ax");
     __asm__("mov %ax, %ds");
@@ -5,62 +7,62 @@ void main() {
     char *vga_con = (char *)0;
     *vga_con = s[0];
     vga_con++;
-    *vga_con = (char)0xf0;
+    *vga_con = WHITE_BG;
     vga_con++;
 
     *vga_con = s[1];
     vga_con++;
-    *vga_con = (char)0xf0;
+    *vga_con = WHITE_BG;
     vga_con++;
 
     *vga_con = s[2];
     vga_con++;
-    *vga_con = (char)0xf0;
+    *vga_con = WHITE_BG;
     vga_con++;
 
     *vga_con = s[3];
     vga_con++;
-    *vga_con = (char)0xf0;
+    *vga_con = WHITE_BG;
     vga_con++;
 
     *vga_con = s[4];
     vga_con++;
-    *vga_con = (char)0xf0;
+    *vga_con = WHITE_BG;
     vga_con++;
 
     *vga_con = s[5];
     vga_con++;
-    *vga_con = (char)0xf0;
+    *vga_con = WHITE_BG;
     vga_con++;
 
     *vga_con = s[6];
     vga_con++;
-    *vga_con = (char)0xf0;
+    *vga_con = WHITE_BG;
     vga_con++;
 
     *vga_con = s[7];
     vga_con++;
-    *vga_con = (char)0xf0;
+    *vga_con = WHITE_BG;
     vga_con++;
 
     *vga_con = s[8];
     vga_con++;
-    *vga_con = (char)0xf0;
+    *vga_con = WHITE_BG;
     vga_con++;
 
     *vga_con = s[9];
     vga_con++;
-    *vga_con = (char)0xf0;
+    *vga_con = WHITE_BG;
     vga_con++;
 
     *vga_con = s[10];
     vga_con++;
-    *vga_con = (char)0xf0;
+    *vga_con = WHITE_BG;
     vga_con++;
 
     *vga_con = s[11];
     vga_con++;
-    *vga_con = (char)0xf0;
+    *vga_con = WHITE_BG;
 
     while (1);
 }
